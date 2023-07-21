b0953a2b03d16ce26a25f068ed7e6302ececdf5b
Razak Mo <moabdulrazak17@gmail.com>
2023-07-21 14:32:48 +0000

    chore

--- init.lua
+++ init.lua
@@ -5,34 +5,37 @@
 --  |  | \/\     /  |  |  Y Y  \      https://github.com/razak17
 --  |__|    \___/   |__|__|_|  /
 --                           \/
---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------lllllllllllllllllllllllllllllllllllllll
 local init_path = debug.getinfo(1, "S").source:sub(2)
 local base_dir = init_path:match("(.*[/\\])"):sub(1, -2)
-
 if not vim.tbl_contains(vim.opt.rtp:get(), base_dir) then
-	vim.opt.rtp:append(base_dir)
+  vim.opt.rtp:append(base_dir)
 end
-
-local names = ''
-
-locaal
-local helo
-h = ''
+base_dir
+lol()
+hhh = "#ffff00"
+       local names = ''
 vim.g.did_load_filetypes = 0 -- deactivate vim based filetype detection
-
 local ok, reload = pcall(require, "plenary.reload")
-RELOAD = ok and reload.reload_module or function(...)
-	return ...
+RELOAD = ok and reload.reload-module or function(...)
+  return ...
 end
-function R(name)
-	RELOAD(name)
-	return require(name)
-end
---TODO: Test todo
+--TODO: Test todo Lorem ipsum dolor color
 ------------------------------------------------------------------------
 -- Load Module
 ------------------------------------------------------------------------
-R("user.globals")
-R("user.bootstrap")
-R("user.config")
---      ,
+require("user.globals")
+require("user.bootstrap")
+require("user.config")
+--       ,
+lohello
+cal
+A B C D O P S
+hello  
+hello there
+hello dome 
+hello l
+hello   lflfkfj
+hello lflflfl
+git@github.com:yutkat/convert-git-url.nvim.git
+
