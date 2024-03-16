feeaab0d89e6dce82e0b3ee494a0a3932c140753
Razak Mo <moabdulrazak17@gmail.com>
2024-03-16 19:58:28 +0000

    chore: add dups.lua

--- dups.lua
+++ dups.lua
@@ -1,18 +1,17 @@
 local function removeDuplicates(t)
-	local seen = {}
-	local result = {}
+  local seen = {}
+  local result = {}
 
-	for _, value in ipairs(t) do
-		if not seen[value] then
-			table.insert(result, value)
-			seen[value] = true
-		end
-	end
+  for _, value in ipairs(t) do
+    if not seen[value] then
+      table.insert(result, value)
+      seen[value] = true
+    end
+  end
 
-	return result
+  return result
 end
 
 local t = { "to", "no", "no", "of" }
 local unique = removeDuplicates(t)
 print(table.concat(unique, ", ")) -- prints "1, 2, 3, 4"
-print(t[2])
